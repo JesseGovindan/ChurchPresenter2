fdfc5855da058bcc455936b3c772761b89a1ecef
Jesse Govindan <jesse.govindan@chillisoft.co.za>
2022-04-14 16:29:13 +0200

    Refine dynamic text scaling on overhead projection

--- frontend/src/projector/OverheadProjector.tsx
+++ frontend/src/projector/OverheadProjector.tsx
@@ -7,27 +7,58 @@ export function OverheadProjector() {
   const folder = useSelector<State, FolderView | null>(
     state => state.serviceManager.selectedFolder);
 
-  const slide = getSlideFromFolder(folder);
+  const [fontScale, setFontScale] = useState(1);
+  const [max, setMax] = useState(1);
+  const d = useRef<HTMLDivElement>(null);
+  const [windowHeight, setWindowHeight] = useState(window.visualViewport.height);
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  useEffect(() => {
+    window.onresize = () => {
+      setWindowHeight(window.visualViewport.height);
+      setWindowWidth(window.visualViewport.width);
+    };
+  }, []);
+
+  useLayoutEffect(() => {
+    setMax(1);
+  }, [folder, windowHeight, windowWidth]);
+
+  useLayoutEffect(() => {
+    const container = d.current;
+    if (container === null) {
+      return;
+    }
+
+    if (container.scrollHeight > windowHeight) {
+      setFontScale(c => c - 0.02);
+      setMax(fontScale - 0.02);
+    } else {
+      setFontScale(c => Math.min(max, c + 0.02));
+    }
+  }, [d, folder, fontScale, max, windowHeight, windowWidth]);
+
+  const slide = getSlideFromFolder(folder, fontScale);
   return (
-    <div className='overhead-projector' data-active={!!slide} data-type={folder?.type}>
+    <div ref={d} className='overhead-projector' data-active={!!slide} data-type={folder?.type}>
       { slide }
     </div>
   );
 }
 
-function getSlideFromFolder(folder: FolderView | null) {
+function getSlideFromFolder(folder: FolderView | null, fontScale: number) {
   if (folder) {
     const visibleSlide = folder.slides.find(s => s.isShown);
     if (visibleSlide) {
       return folder.type === 'lyric' ?
-        <LyricSlide folder={folder}/> :
-        <ScriptureSlide slide={visibleSlide} />;
+        <LyricSlide folder={folder} fontScale={fontScale}/> :
+        <ScriptureSlide slide={visibleSlide} fontScale={fontScale}/>;
     }
   }
   return null;
 }
 
-function LyricSlide(props: {folder: FolderView}) {
+function LyricSlide(props: {folder: FolderView, fontScale: number}) {
   const slides = props.folder.slides;
   const showingIndex = slides.findIndex(s => s.isShown);
   const currentSection = slides[showingIndex].sectionName;
@@ -51,6 +82,9 @@ function LyricSlide(props: {folder: FolderView}) {
     }
   }
 
+  const verseFontSize = `${(6*props.fontScale).toFixed(2)}vmax`;
+  console.log(verseFontSize);
+
   const parts = slides.slice(startIndex, endIndex)
     .flatMap(slide => slide.text.split('\n').map(line => ({
       text: line,
@@ -58,50 +92,23 @@ function LyricSlide(props: {folder: FolderView}) {
       caption: '',
     })))
     .map((slide, index) => {
-      return <div key={index}>{slide.text}</div>;
+      return <div key={index} style={{fontSize: verseFontSize}}>{slide.text}</div>;
     });
 
   return <div className='lyric'>{parts}</div>;
 }
 
-function ScriptureSlide(props: {slide: SlideView}) {
-  const [fontSize, setFontSize] = useState(7);
-  const [max, setMax] = useState(7);
-  const d = useRef<HTMLDivElement>(null);
-  const [windowHeight, setWindowHeight] = useState(window.visualViewport.height);
-  const [windowWidth, setWindowWidth] = useState(0);
-
-  useEffect(() => {
-    window.onresize = () => {
-      setWindowHeight(window.visualViewport.height);
-      setWindowWidth(window.visualViewport.width);
-    };
-  }, []);
-
-  useLayoutEffect(() => {
-    setMax(7);
-  }, [props.slide, windowHeight, windowWidth]);
-
-  useLayoutEffect(() => {
-    const container = d.current;
-    if (container === null) {
-      return;
-    }
-
-    if (container.scrollHeight > windowHeight) {
-      setFontSize(c => c - 0.1);
-      setMax(fontSize - 0.1);
-    } else {
-      setFontSize(c => Math.min(max, c + 0.1));
-    }
-  }, [d, props.slide, fontSize, windowHeight, windowWidth]);
+function ScriptureSlide(props: {slide: SlideView, fontScale: number}) {
+  const captionFontSize = `${(4*props.fontScale).toFixed(2)}vmax`;
+  const verseFontSize = `${(6*props.fontScale).toFixed(2)}vmax`;
+  console.log(captionFontSize);
 
   return (
-    <div ref={d} className='scripture'>
-      <div className='caption' style={{fontSize: `${fontSize - 1}vmin`}}>
+    <div className='scripture'>
+      <div className='caption' style={{fontSize: captionFontSize}}>
         {props.slide?.caption}
       </div>
-      <div className='verse' style={{fontSize: `${fontSize}vmin`}}>{props.slide.text}</div>
+      <div className='verse' style={{fontSize: verseFontSize}}>{props.slide.text}</div>
     </div>
   );
 }
