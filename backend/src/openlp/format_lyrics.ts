89c7a109c3890c26a47deb7e6beea72e8d876109
Jesse Govindan <jesse.govindan@chillisoft.co.za>
2022-04-22 20:27:30 +0200

    Refactor format_lyrics.ts

--- backend/src/openlp/format_lyrics.ts
+++ backend/src/openlp/format_lyrics.ts
@@ -5,20 +5,28 @@ export interface LyricSlide {
 }
 
 export function formatLyrics(slides: LyricSlide[]): { xml: string, searchLyrics: string } {
-  const xmlSlides = slides.map(slide => {
-    const startTag = `<verse label="${slide.label}" type="${slide.type}">`
-    const endTag = `</verse>`
-    const inner = `<![CDATA[${slide.lyrics}]]>`
-    return startTag + inner + endTag
-  })
-
-  const header = `<?xml version='1.0' encoding='UTF-8'?><song version="1.0"><lyrics>`
-  const footer = `</lyrics></song>`
-
   return { 
-    xml: header + xmlSlides.join('') + footer,
-    searchLyrics: slides.map(slide =>
-      slide.lyrics.toLowerCase().replace(/\n/g, ' ').replace(/[,\.!\@#\$%\^&\*]/g, '')
-    ).join(' '),
+    xml: createXmlLyrics(slides),
+    searchLyrics: createSearchLyrics(slides),
   }
+}
+
+const XML_LYRIC_HEADER = `<?xml version='1.0' encoding='UTF-8'?><song version="1.0"><lyrics>`
+const XML_LYRIC_FOOTER = `</lyrics></song>`
+
+function createXmlLyrics(slides: LyricSlide[]): string {
+  const xmlLyrics = slides.map(slide => {
+    const startTag = `<verse label="${slide.label}" type="${slide.type}">`
+    const innerData = `<![CDATA[${slide.lyrics}]]>`
+    const endTag = `</verse>`
+    return startTag + innerData + endTag
+  }).join('')
+
+  return XML_LYRIC_HEADER + xmlLyrics + XML_LYRIC_FOOTER
+}
+
+function createSearchLyrics(slides: LyricSlide[]): string {
+  return slides.map(slide =>
+    slide.lyrics.toLowerCase().replace(/\n/g, ' ').replace(/[,\.!\@#\$%\^&\*]/g, '')
+  ).join(' ')
 }
