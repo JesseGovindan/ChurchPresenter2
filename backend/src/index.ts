9c103492a8c22e630e9ee585c2ac18f47d03c918
Jesse Govindan <jesse.govindan@chillisoft.co.za>
2022-04-22 20:18:20 +0200

    Refactor backend index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,8 @@ import { initialiseSongsDatabase } from './db/sqlite'
 import { createServer } from './server'
 
 function main() {
-  const config = getConfig(process.argv.slice(2))
+  const processArguments = process.argv.slice(2)
+  const config = getConfig(processArguments)
   initialiseSongsDatabase(config.songDatabaseLocation)
   console.log('Song Database Location: ', config.songDatabaseLocation)
   const server = startBackend(config)
@@ -22,20 +23,19 @@ function startBackend(config: Config) {
 
 function startFrontend(config: Config, server: http.Server) {
   if (!config.onlyApi) {
-    app.on('ready', async () => {
-      const window = new BrowserWindow({
-        width: 400,
-        height: 600,
-        icon: path.join(__dirname, 'public/favicon.ico'),
-      })
-      window.loadURL(`http://localhost:${config.serverPort}/mobile`)
-      window.menuBarVisible = false
-    })
-
-    app.on('before-quit', () => {
-      server.close()
-    })
+    app.on('ready', () => createBrowserWindow(config))
+    app.on('before-quit', () => server.close())
   }
+}
+
+function createBrowserWindow(config: Config) {
+  const window = new BrowserWindow({
+    width: 400,
+    height: 600,
+    icon: path.join(__dirname, 'public/favicon.ico'),
+  })
+  window.loadURL(`http://localhost:${config.serverPort}/mobile`)
+  window.menuBarVisible = false
 }
 
 main()
